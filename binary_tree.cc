f80e9b1f44b32a393577d00b36a1ea30e63eca49
Chris <namtaerg@gmail.com>
2024-01-15 11:41:22 +0800

    添加vscode的编译设置；首次二叉树类代码；

---
+++ binary_tree.cc
@@ -0,0 +1,19 @@
+#include "binary_tree.h"
+
+#include <iostream>
+#include <stack>
+#include <list>
+
+namespace test {
+
+BinaryTree::BinaryTree(const std::vector<int>& data) {
+}
+
+std::vector<int> BinaryTree::PreOrderTraversal(TreeNode* root) {
+}
+
+bool BinaryTree::CreateBinaryTree(const std::vector<int>& data) {
+
+}
+
+}  // namespace test
---
+++ binary_tree.h
@@ -0,0 +1,33 @@
+#ifndef TEST_BINARY_TREE_H_
+#define TEST_BINARY_TREE_H_
+
+#include <iostream>
+#include <stack>
+#include <list>
+#include <vector>
+
+namespace test {
+
+struct TreeNode {
+   int val;
+   struct TreeNode *left;
+   struct TreeNode *right;
+   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+  
+class BinaryTree {        
+ public:
+	BinaryTree() = default;
+	BinaryTree(const std::vector<int>& data);
+
+	bool CreateBinaryTree(const std::vector<int>& data);
+
+	std::vector<int> PreOrderTraversal(TreeNode* root);
+
+ private:
+  BinaryTree* root_;
+};
+
+}  // namespace test
+
+#endif  // TEST_BINARY_TREE_H_
